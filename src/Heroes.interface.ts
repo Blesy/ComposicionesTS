c2a48a6e999103d2696f158206b6e54bea6e6633
Blesy <40645486+Blesy@users.noreply.github.com>
2020-08-18 17:57:41 -0600

    Add files via upload

---
+++ src/Heroes.interface.ts
@@ -0,0 +1,179 @@
+export interface Heroes
+{
+    Nombre: string,
+    Rasgo1: string,
+    Rasgo2: string,
+    Rasgo3: string,
+}
+export interface Rasgos
+{
+    crono: 
+    {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    cibernetico: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    guardiana: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    estrella: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    celestial: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    rebelde: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    mecha: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    pirata: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    astro: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    guerra: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    pirotecnico: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    espadachin: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    artillero: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    luchador: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    infiltrado: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    segador: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    mercenario: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    mistico: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    protector: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    vanguardia: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    tirador: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    destructor: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    hechicero: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    },
+    ejemplar: {
+        contador: number,
+        oro: number,
+        plata: number,
+        bronce: number,
+        espatula: boolean
+    }
+}
