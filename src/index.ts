246cb730cfaabe57c1acdd069e6216e197caba38
Blesy <blesyangulo@gmail.com>
2023-08-02 17:27:22 -0700

    set 9 update

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,38 @@ import * as fs from 'fs';
 
 let lista: Heroes[] = {...require('./datos/lista.json')};
 const rasgos: Rasgos = {...require('./datos/rasgos2.json')};
+const TIER_LIST: String[] = require('./datos/tierList.json')
 let largo = Object.keys(lista).length;
 let contador: Rasgos = {...rasgos};
 var oro: number = 0, plata: number = 0, bronce: number = 0, sumaRasgos: number = 0, id: number = 0, espatula: string = "";
 var obj = {
     table: []
 };
-for (let i = 0; i < largo - 8; i++) 
+for (let i = 0; i < largo - 9; i++) 
 {
-    for (let j = i+1; j < largo - 7; j++) 
+    for (let j = i+1; j < largo - 8; j++) 
     {
-        for (let k = j+1; k < largo - 6; k++) 
+        for (let k = j+1; k < largo - 7; k++) 
         {
-            for (let l = k+1; l < largo - 5; l++) 
+            for (let l = k+1; l < largo - 6; l++) 
             {
-                for (let m = l+1; m < largo - 4; m++) 
+                for (let m = l+1; m < largo - 5; m++) 
                 {
-                    for (let n = m+1; n < largo - 3; n++) 
+                    for (let n = m+1; n < largo - 4; n++) 
                     {
-                        for (let o = n+1; o < largo - 2; o++) 
+                        for (let o = n+1; o < largo - 3; o++) 
                         {
-                            for (let p = o+1; p < largo - 1; p++) 
+                            for (let p = o+1; p < largo - 2; p++) 
                             {
-                                for (let q = p + 1; q < largo; q++) 
+                                for (let q = p + 1; q < largo - 1; q++) 
                                 {
-                                    limpiar();
-                                    let composicion: Heroes[] = componer(i, j, k, l, m, n, o, p, q);
-                                    sumar(composicion);
-                                    if (sumaRasgos > 17){
-                                        impresion(composicion);
+                                    for (let r = q + 1; r < largo; r++) {
+                                        limpiar();
+                                        let composicion: Heroes[] = componer(i, j, k, l, m, n, o, p, q, r);
+                                        sumar(composicion);
+                                        if (sumaRasgos > 21){
+                                            impresion(composicion);
+                                        }
                                     }
                                 }
                             }
@@ -52,7 +55,7 @@ function impresion(compo: Heroes[]){
         espatula = "vacio";
     }
     id++;
-    for (let index = 0; index < 9; index++) {
+    for (let index = 0; index < compo.length; index++) {
         cadena += `${compo[index].Nombre}, `;
     }
     let principal: string = "", principalContador: number = 0;
@@ -76,6 +79,7 @@ function impresion(compo: Heroes[]){
         siete: compo[6].Nombre,
         ocho: compo[7].Nombre,
         nueve: compo[8].Nombre,
+        diez: compo[9].Nombre,
         oro: oro,
         plata: plata,
         bronce: bronce,
@@ -97,9 +101,11 @@ function limpiar(){
     sumaRasgos = 0;
 }
 function sumar(compo: Heroes[]) {
+    const containTierS = compo.some(obj => TIER_LIST.includes(obj.Nombre))
+    if (!containTierS) return
     let rasgo1: string, rasgo2: string, rasgo3: string;
     let summ: number, Acumoro: number, Acumplata: number, Acumbronce: number;
-    for (let index = 0; index < 9; index++) {
+    for (let index = 0; index < compo.length; index++) {
         rasgo1 = compo[index].Rasgo1;
         rasgo2 = compo[index].Rasgo2;
         rasgo3 = compo[index].Rasgo3;
@@ -114,11 +120,11 @@ function sumar(compo: Heroes[]) {
         Acumoro = contador[key].oro;
         Acumplata = contador[key].plata;
         Acumbronce = contador[key].bronce;
-        if (contador[key].espatula === true && espatula === "" && (summ === Acumoro - 1 || summ === Acumplata - 1 || summ === Acumbronce - 1)){
-            contador[key].contador++;
-            summ++;
-            espatula = key;
-        }
+        // if (contador[key].espatula === true && espatula === "" && (summ === Acumoro - 1 || summ === Acumplata - 1 || summ === Acumbronce - 1)){
+        //     contador[key].contador++;
+        //     summ++;
+        //     espatula = key;
+        // }
         if (summ >= Acumoro){
             oro++;
             sumaRasgos += summ;
@@ -134,9 +140,9 @@ function sumar(compo: Heroes[]) {
     });
 }
 
-function componer(i: number, j: number, k: number, l: number, m: number, n: number, o: number, p: number, q: number) {
-    let listaContador: number[] = [i, j, k, l, m, n, o, p, q];
-    let composicion: Heroes[] = [null, null, null, null, null, null, null, null, null];
+function componer(i: number, j: number, k: number, l: number, m: number, n: number, o: number, p: number, q: number, r: number) {
+    let listaContador: number[] = [i, j, k, l, m, n, o, p, q, r];
+    let composicion: Heroes[] = [null, null, null, null, null, null, null, null, null, null];
     for (let index = 0; index < listaContador.length; index++) {
         composicion[index] = lista[listaContador[index]];
     }
